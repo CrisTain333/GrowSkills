c57162894b3d250097d1d6101272af9fcb7ad40d
Cristain <mitashil99@gmail.com>
2022-10-10 19:52:58 +0400

    Header Added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Main from "./Layout/Main.jsx";
+import ErrorPage from './Components/error/ErrorPage'
+import Home from "./Components/Home/Home.jsx";
 function App() {
   const router = createBrowserRouter([
     {
       path: "/",
       element: <Main />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+      ],
     },
+    {
+      path:'*',
+      element:<ErrorPage/>
+    }
   ]);
   return (
     <>
---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Home.css";
+
+const Home = () => {
+  return (
+    <>
+    <div  className="header"> 
+    <div>
+      <h1>Let's Take Your Skill To Next Level</h1>
+    </div>
+    </div>
+    </>
+    
+  );
+};
+
+export default Home;
